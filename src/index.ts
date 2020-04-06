ae033849a7ae4c8d1cda7eea54b2bac69ad60176
justin0979 <justinm0979@gmail.com>
2020-04-06 15:50:20 -0500

    updated program to use typescript

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import "index.html";
+import "main.scss";
+
+interface Sayings {
+  text: string;
+  title: string;
+}
+
+const sayings: Sayings = {
+  text: "This interface works with typescript!",
+  title: "Typescript Front End",
+};
+
+const app = document.querySelector(".app");
+
+const title = document.createElement("h1");
+title.innerHTML = sayings.title;
+app.appendChild(title);
+
+const saying = document.createElement("h2");
+saying.innerHTML = sayings.text;
+app.appendChild(saying);
