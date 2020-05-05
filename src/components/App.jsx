c7b1f400a829c12d974333822fa1d63fcf8a27eb
justin0979 <justinm0979@gmail.com>
2020-05-05 13:59:26 -0500

    add Dockerfile.dev, docker-compose.yml, commented out devserver.public in webpack config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, Suspense, lazy } from "react";
+const Lazy = lazy(() => import("./Lazy"));
 
 const App = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  const lazyLoadLazy = () => {
+    if (loaded) {
+      return (
+        <Suspense fallback="Lazy loading Lazy component">
+          <Lazy />
+        </Suspense>
+      );
+    }
+    return <h1>Loaded here</h1>;
+  };
+
   return (
     <div className="app">
       <h1>Basic React App Component</h1>
+      {lazyLoadLazy()}
+      <button onClick={() => setLoaded(!loaded)}>lazy load</button>
     </div>
   );
 };
---
+++ src/components/Lazy.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1>I think I'll just be lazy loaded</h1>
+    </div>
+  );
+};
