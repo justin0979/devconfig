789ce735a8cadaa473bc5f6f2cd0707dcaf34699
justin0979 <justinm0979@gmail.com>
2020-07-26 21:16:10 -0500

    add webpack favicons plugin and upgraded html webpack plugin, add personal favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   entry: ["./src"],
@@ -73,5 +74,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new FaviconsWebpackPlugin("./public/favicon-32x32.png"),
   ],
 };
