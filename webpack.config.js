d8d90e98e695cb6ea823ffb53875b22905bb14a0
justin0979 <justinm0979@gmail.com>
2020-07-27 14:47:46 -0500

    change webpack publicPath from '/' to '' due to routing issues on build

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/", // without /, kept getting 404 errors when refreshing with redux-form
+    publicPath: "", // without /, kept getting 404 errors when refreshing with redux-form
   },
   mode: "development",
   optimization: {
