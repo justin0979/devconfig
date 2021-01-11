eac91d10676cc4e1d24f5c25ff5c889254a70837
justin0979 <justinm0979@gmail.com>
2021-01-11 15:11:27 -0600

    run npm update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const saying2 = document.createElement("h2");
 saying2.innerHTML = "webpack-dev-server";
 
 const saying = document.createElement("h2");
-saying.innerHTML = "Works with Webpack 5";
+saying.innerHTML = "Working with Webpack 5";
 const app = document.querySelector(".app");
 app.appendChild(saying2);
 app.appendChild(saying);
