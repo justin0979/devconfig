7bb7dfa60ac99bcf47982ead5369dca7980c4d32
justin0979 <justinm0979@gmail.com>
2021-04-15 12:40:32 -0500

    add output of process.env.NODE_ENV to DOM, run npm update

--- src/index.js
+++ src/index.js
@@ -9,3 +9,9 @@ saying.innerHTML = "Working with Webpack 5";
 const app = document.querySelector(".app");
 app.appendChild(saying2);
 app.appendChild(saying);
+
+const environment = document.createElement("h3");
+environment.innerHTML = `Environment = ${process.env.NODE_ENV}`;
+
+const module_hot = document.createElement("h3");
+app.appendChild(environment);
