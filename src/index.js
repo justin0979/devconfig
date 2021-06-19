55e3501d0703f56b0bb5645421ba9749ceaea17c
justin0979 <justinm0979@gmail.com>
2021-06-18 19:28:55 -0500

    add list and reminder to remove axios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
+import axios from "axios";
 import "../public/index.html";
 import "./main.scss";
 
-const saying2 = document.createElement("h2");
-saying2.innerHTML = "webpack-dev-server";
+const starting = async () => {
+  const response = await axios.get(
+    "https://jsonPlaceholder.typicode.com/users",
+  );
 
-const saying = document.createElement("h2");
-saying.innerHTML = "Working with Webpack 5";
-const app = document.querySelector(".app");
-app.appendChild(saying2);
-app.appendChild(saying);
+  const users = response.data;
 
-const environment = document.createElement("h3");
-environment.innerHTML = `Environment = ${process.env.NODE_ENV}`;
+  const ul = document.createElement("ul");
 
-const module_hot = document.createElement("h3");
-app.appendChild(environment);
+  const renderUsers = users.forEach((user) => {
+    const li = document.createElement("li");
+    li.key = user.id;
+    li.innerHTML = user.name;
+    ul.appendChild(li);
+  });
+
+  const saying2 = document.createElement("h2");
+  saying2.innerHTML = "webpack-dev-server";
+
+  const saying = document.createElement("h2");
+  saying.innerHTML = "Working with Webpack 5";
+  const app = document.querySelector(".app");
+  app.appendChild(saying2);
+  app.appendChild(saying);
+
+  const environment = document.createElement("h3");
+  environment.innerHTML = `Environment = ${process.env.NODE_ENV}`;
+
+  const module_hot = document.createElement("h3");
+  app.appendChild(environment);
+
+  app.appendChild(ul);
+
+  const reminder = `run "npm uninstall axios" if it is not needed.`;
+  const p_reminder = document.createElement("p");
+  p_reminder.innerHTML = reminder;
+  app.appendChild(p_reminder);
+};
+
+starting();
