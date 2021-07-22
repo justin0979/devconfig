cfd7bb03d643f31cac1d995974fffed09918afcd
justin0979 <justinm0979@gmail.com>
2021-07-21 23:58:45 -0500

    add main.scss to webpack prod entry

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,7 +4,7 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 
 module.exports = merge(commonConfig, {
-  entry: ["./src"],
+  entry: ["./src", "./src/sass/main.scss"],
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "../dist"),
