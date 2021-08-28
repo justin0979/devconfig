17a29f86fa8ffff9bb1a7f4076c5320dc11d472e
justin0979 <justinm0979@gmail.com>
2021-08-28 15:52:11 -0500

    refactor .btn to be wider on hover

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from '&components/App';
-import './sass/main.scss';
+import '&sass/main.scss';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
