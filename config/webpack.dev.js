6093e5a06b05b0f83105d2958f62303faa8bb582
justin <justinm0979@gmail.com>
2022-02-04 16:27:00 -0600

    rm eslint from fork-ts-checker, update npm packages

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TsconfigPathsWebpackPlugin = require('tsconfig-paths-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const StylelintPlugin = require('stylelint-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TsconfigPathsWebpackPlugin = require("tsconfig-paths-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
-  entry: ['./src/index.ts'],
+  entry: ["./src/index.ts"],
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    publicPath: '/',
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/",
   },
-  mode: 'development',
-  devtool: 'eval',
+  mode: "development",
+  devtool: "eval",
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
     },
   },
   devServer: {
     port: 3000,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     client: {
       overlay: {
         errors: true,
@@ -30,37 +30,37 @@ module.exports = {
     },
   },
   resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
     plugins: [
       new TsconfigPathsWebpackPlugin({
-        configFile: 'tsconfig.json',
+        configFile: "tsconfig.json",
       }),
     ],
     extensions: [
-      '.js',
-      '.jsx',
-      '.ts',
-      '.tsx',
-      '.css',
-      '.scss',
-      '.jpg',
-      '.jpeg',
-      '.png',
-      '.svg',
-      '.gif',
-      '.ttf',
-      '.woff',
-      '.woff2',
-      '.eot',
-      'mp4',
-      'webm',
+      ".js",
+      ".jsx",
+      ".ts",
+      ".tsx",
+      ".css",
+      ".scss",
+      ".jpg",
+      ".jpeg",
+      ".png",
+      ".svg",
+      ".gif",
+      ".ttf",
+      ".woff",
+      ".woff2",
+      ".eot",
+      "mp4",
+      "webm",
     ],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
@@ -68,7 +68,7 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
               transpileOnly: true,
             },
@@ -79,18 +79,18 @@ module.exports = {
         test: /\.s?css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
       },
       {
         test: /\.html$/,
-        loader: 'html-loader',
+        loader: "html-loader",
       },
       {
         test: /\.(jpe?g|png|gif|svg|ttf|woff(2)?|eot|mp4|webm)$/,
-        type: 'asset',
+        type: "asset",
         parser: {
           dataUrlCondition: {
             maxSize: 3 * 1024, // 3 kBs
@@ -101,17 +101,14 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/lastname-32x32.png',
+      template: "./public/index.html",
+      favicon: "./public/lastname-32x32.png",
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      filename: "styles.css",
     }),
     new CleanWebpackPlugin(),
     new ForkTsCheckerWebpackPlugin({
-      eslint: {
-        files: './src/**/*.{ts,tsx,js,jsx}',
-      },
       typescript: {
         diagnosticOptions: {
           semantic: true,
