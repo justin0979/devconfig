f37cfae63809b99b9c0796fc78587b99da17b267
justin0979 <justinm0979@gmail.com>
2023-02-10 18:21:58 -0600

    add no empty block and 2 font-family rules to css linter

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     "at-rule-empty-line-before": null,
     "at-rule-no-unknown": null,
+    "block-no-empty": null,
     "color-hex-length": null,
     "declaration-colon-newline-after": null,
     "declaration-empty-line-before": [
@@ -17,5 +18,7 @@ module.exports = {
         ignore: ["inside-parens"],
       },
     ],
+    "font-family-name-quotes": null,
+    "font-family-no-missing-generic-family-keyword": null,
   },
 };
