eeead85568f4c3659b013ccf825deaf8791178bb
justin0979 <justinm0979@gmail.com>
2023-02-19 12:40:09 -0600

    add png to Lazy with clickable div

--- src/components/Lazy.jsx
+++ src/components/Lazy.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import MyPic from "./my_name-32x32.png";
+
 const Lazy = () => {
+  const [comment, setComment] = useState("");
+
+  const handleClick = (event) => {
+    setComment("Who??? And Where???");
+  };
+
   return (
-    <div>
+    <div className="lazy">
       <h1>I think I'll just be lazy loaded</h1>
+      <img className="lazy__pic" src={MyPic} alt="GIMP work of art" />
+      <div onClick={handleClick} className="comment">
+        {comment}
+      </div>
     </div>
   );
 };
