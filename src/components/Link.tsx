1d8b3a492626079d8ca290cbc68149fbc790a10f
justin0979 <justinm0979@gmail.com>
2023-02-19 13:30:34 -0600

    add condition if meta or cntrl is pressed with link

---
+++ src/components/Link.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import { useNavigation } from "../hooks";
+
+interface LinkProps {
+  to: string;
+  children: ReactNode;
+}
+
+const Link = ({ to, children }: LinkProps) => {
+  const { navigate } = useNavigation();
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+    event.preventDefault();
+
+    navigate(to);
+  };
+
+  return (
+    <a onClick={handleClick} href={to}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
