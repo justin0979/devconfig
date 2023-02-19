4f75e4c667641abb88bef99d38ca6bf56011e528
justin0979 <justinm0979@gmail.com>
2023-02-19 12:13:44 -0600

    install and setup react-refresh-webpack plugin for hmr

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,33 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const StylelintPlugin = require('stylelint-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 //const CopyWebpackPlugin = require("copy-webpack-plugin");
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 module.exports = {
+  mode: isDevelopment ? "development" : "production",
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
     },
   },
   resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
     extensions: [
-      '.js',
-      '.jsx',
-      '.css',
-      '.scss',
-      '.jpg',
-      '.jpeg',
-      '.png',
-      '.svg',
-      '.gif',
-      '.ttf',
-      '.woff',
-      '.woff2',
-      '.eot',
-      'mp4',
-      'webm',
+      ".js",
+      ".jsx",
+      ".css",
+      ".scss",
+      ".jpg",
+      ".jpeg",
+      ".png",
+      ".svg",
+      ".gif",
+      ".ttf",
+      ".woff",
+      ".woff2",
+      ".eot",
+      "mp4",
+      "webm",
     ],
   },
   module: {
@@ -35,15 +39,24 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              plugins: [
+                isDevelopment && require.resolve("react-refresh/babel"),
+              ].filter(Boolean),
+            },
+          },
+        ],
       },
       {
         test: /\.html$/,
-        use: 'html-loader',
+        use: "html-loader",
       },
       {
         test: /\.(jpe?g|png|gif|svg|ttf|woff(2)?|eot|mp4|webm)$/,
-        type: 'asset',
+        type: "asset",
         parser: {
           dataUrlCondition: {
             maxSize: 3 * 1024, // 3 kBs
@@ -54,14 +67,15 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon-32x32.png',
+      template: "./public/index.html",
+      favicon: "./public/favicon-32x32.png",
       meta: {
-        viewport: 'width=device-width, initial-scale=1',
+        viewport: "width=device-width, initial-scale=1",
       },
     }),
     new CleanWebpackPlugin(),
     new StylelintPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     //    new CopyWebpackPlugin({
     //      patterns: [
     //        {
@@ -69,5 +83,5 @@ module.exports = {
     //        },
     //      ],
     //    }),
-  ],
+  ].filter(Boolean),
 };
