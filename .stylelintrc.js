6a7be7e060d3aa15a3fd60cc01106d5a456c4515
justin0979 <justinm0979@gmail.com>
2023-03-01 15:56:24 -0600

    add rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "at-rule-empty-line-before": null,
     "at-rule-no-unknown": null,
     "color-hex-length": null,
+    "comment-empty-line-before": null,
     "declaration-colon-newline-after": null,
     "declaration-empty-line-before": [
       "never",
@@ -17,6 +18,9 @@ module.exports = {
         ignore: ["inside-parens"],
       },
     ],
+    "font-family-name-quotes": null,
+    "font-family-no-missing-generic-family-keyword": null,
+    "import-notation": null,
     "scss/at-mixin-argumentless-call-parentheses": null,
     "scss/double-slash-comment-empty-line-before": null,
     "scss/dollar-variable-empty-line-before": null,
