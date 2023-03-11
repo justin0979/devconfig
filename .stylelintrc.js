a97f99acbe0940b1726674ebfc39aae1dfe3b981
justin0979 <justinm0979@gmail.com>
2023-03-10 18:29:35 -0600

    add rules that threw errors in other projects

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,23 +1,30 @@
 module.exports = {
   extends: "stylelint-config-standard-scss",
   rules: {
+    "alpha-value-notation": null,
     "at-rule-empty-line-before": null,
     "at-rule-no-unknown": null,
+    "block-no-empty": null,
+    "color-function-notation": null,
     "color-hex-length": null,
     "comment-empty-line-before": null,
+    "custom-property-empty-line-before": null,
     "declaration-colon-newline-after": null,
-    "declaration-empty-line-before": [
-      "never",
-      {
-        ignore: ["after-declaration"],
-      },
-    ],
+    "declaration-block-no-redundant-longhand-properties": null,
+    "declaration-empty-line-before": null,
     indentation: [
       2,
       {
         ignore: ["inside-parens"],
       },
     ],
+    "font-family-name-quotes": null,
+    "font-family-no-missing-generic-family-keyword": null,
+    "import-notation": null,
+    "no-descending-specificity": null,
+    "no-empty-source": null,
+    "no-invalid-double-slash-comments": null,
+    "value-keyword-case": null,
     "scss/at-mixin-argumentless-call-parentheses": null,
     "scss/dollar-variable-empty-line-before": null,
     "scss/double-slash-comment-empty-line-before": null,
