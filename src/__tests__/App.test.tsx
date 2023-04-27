be027520bd8267ce84516a28b0c45526d3daacdc
justin0979 <justinm0979@gmail.com>
2023-04-27 13:15:45 -0500

    update testing to use react testing library

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,23 +1,9 @@
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "../App";
 
-let container: any = null;
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
 it("renders with a heading", () => {
-  act(() => {
-    render(<App />, container);
-  });
-  const heading = container.querySelector("h1");
-  expect(heading.textContent).toBe("With Typescript");
+  render(<App />);
+  const div = screen.getByTestId("apptest");
+  expect(div).not.toBeNull;
 });
