06645411053cb2a3886ed9e96c85bffececcab5f
justin0979 <justinm0979@gmail.com>
2023-06-02 16:10:40 -0500

    spread out ReactDOM code; upgrade npm packages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "components/App";
 import "./sass/main.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
 
 //  Comment out below after webpack-dev-server v4.1.0
 //  if (module.hot) {
