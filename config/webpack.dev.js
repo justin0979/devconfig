743ed5c0feebfb27a94d772328afe6433c390ee6
justin0979 <justinm0979@gmail.com>
2023-07-08 16:13:30 -0500

    use chunkFilename prop in css plugin

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,13 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common");
 
 module.exports = merge(commonConfig, {
-  entry: ['./src/index'],
+  entry: ["./src/index"],
   output: {
-    publicPath: '',
+    publicPath: "",
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 3000,
     // host: "0.0.0.0"  // add for docker use
@@ -28,16 +28,16 @@ module.exports = merge(commonConfig, {
         test: /\.s?css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
       },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      chunkFilename: "my-styles.css",
     }),
   ],
 });
