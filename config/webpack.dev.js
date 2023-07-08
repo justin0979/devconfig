2816131b378d2662b4db2eb613ba65c75c1e7a39
justin0979 <justinm0979@gmail.com>
2023-07-08 16:22:27 -0500

    change filename to chunkFilename in css plugin

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,24 +1,24 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common");
 
 module.exports = merge(commonConfig, {
-  entry: ['./src'],
+  entry: ["./src"],
   output: {
-    publicPath: '',
+    publicPath: "",
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
     client: {
       overlay: true,
       progress: true,
-      webSocketTransport: 'ws',
+      webSocketTransport: "ws",
     },
     hot: true,
     port: 3000,
-    host: '0.0.0.0', // add for docker
+    host: "0.0.0.0", // add for docker
     historyApiFallback: true,
     client: {
       overlay: {
@@ -34,33 +34,30 @@ module.exports = merge(commonConfig, {
         test: /\.s?css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
+          "css-loader",
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
-                config: path.resolve(
-                  __dirname,
-                  'postcss.config.js',
-                ),
+                config: path.resolve(__dirname, "postcss.config.js"),
               },
             },
           },
-          'sass-loader',
+          "sass-loader",
         ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/lastname-32x32.png',
+      template: "./public/index.html",
+      favicon: "./public/lastname-32x32.png",
       meta: {
-        viewport: 'width=device-width, initial-scale=1',
+        viewport: "width=device-width, initial-scale=1",
       },
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      chunkFilename: "my-styles.css",
     }),
   ],
 });
