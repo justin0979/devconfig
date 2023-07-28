604ea92c3b588578c0f1419f331d2bd3aa184060
justin0979 <justinm0979@gmail.com>
2023-07-28 12:52:48 -0500

    refactor returned jsx to be set to variable, remove comments

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,40 +1,28 @@
-// import { useDispatch } from "react-redux";
-// import { useAppSelector } from "../hooks";
-// import { fetchLists, deleteList } from "../state";
-import { useState, ReactNode } from "react";
+import { ReactNode } from "react";
 import { useFetchUsersQuery, User } from "../store";
 
 const Users: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
   const { data, error, isFetching } = useFetchUsersQuery();
-  // const dispatch = useDispatch();
-  // const users = useAppSelector((state) => state.users);
 
-  // const getUsersList: React.MouseEventHandler<
-  //   HTMLButtonElement
-  // > = () => {
-  //     dispatch(fetchLists("users"));
+  let content: ReactNode;
 
-  // };
-
-  // const emptyUsersList: React.MouseEventHandler<
-  //   HTMLButtonElement
-  // > = () => {
-  //      dispatch(deleteList());
-  // };
-
-  const renderUsers = (): ReactNode => {
-    return (
+  if (error) {
+    content = (
+      <div>There seems to be an error with getting the users.</div>
+    );
+  } else if (isFetching) {
+    content = <div>Delivering you those names.</div>;
+  } else {
+    content =
       data &&
       data.map((user) => {
         return <li key={user.id}>{user.name}</li>;
-      })
-    );
-  };
+      });
+  }
 
   return (
     <section className="section__users">
-      <ol className="section__users--list">{renderUsers()}</ol>
+      <ol className="section__users--list">{content}</ol>
     </section>
   );
 };
