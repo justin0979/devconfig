fd72b1fbd4826a53f06f0661ae4521367575ec3d
justin0979 <justinm0979@gmail.com>
2023-09-25 13:40:57 -0500

    test: add two assertions for anchor tag identification

--- src/pages/HomePage.test.tsx
+++ src/pages/HomePage.test.tsx
@@ -4,6 +4,13 @@ import HomePage from "./HomePage";
 test("One anchor exists", () => {
   render(<HomePage />);
 
-  const anchor = screen.getAllByRole("link");
-  expect(anchor).toHaveLength(1);
+  const anchors = screen.getAllByRole("link");
+  expect(anchors).toHaveLength(1);
+  expect(anchors[0]).toHaveAttribute(
+    "href",
+    "https://medium.com/@dahvinchee/how-to-set-up-a-react-typescript-project-from-scratch-with-babel-and-webpack-6b069881229d",
+  );
+  expect(anchors[0]).toHaveTextContent(
+    "There are lots of articles with explanations (like this one) for setup, like the tsconfig.json",
+  );
 });
